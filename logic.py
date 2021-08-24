fa70bf2bd499e49d20702f7a1cc69dfb8e9b428f
Sterben404 <43511954+sterben404@users.noreply.github.com>
2021-08-24 12:22:47 +0800

    Add files via upload

---
+++ logic.py
@@ -0,0 +1,12 @@
+def logic(n):
+    arr = []
+    c = 15
+    for x in range(0,20):
+        x += 1
+        c += 15
+        if x == 15:
+            print('BMG')
+        arr.append(x)
+        print(x)
+    
+logic(20)
