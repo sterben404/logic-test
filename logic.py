d0aa623a8188d335464cb6e1eee0752e8ff02510
Sterben404 <43511954+sterben404@users.noreply.github.com>
2021-08-25 18:17:56 +0800

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,18 +1,22 @@
 ## SOLVED ##
-
-def logic(n): 
+global n
+def logic(n):
     hasil = 0
     for x in range(0,n):
         x += 1
         count = [i for i in range(0,n,15)]
         s = [i - 1 for i in range(0,n,15)]
         hasil = hasil + x
+        
         if x in count:
             print('BMG',end=",")
+            continue
         if x in s:
             print(x, end=",")
             continue
-	        
+        if x == n:
+            print(x, end="")
+            continue
         print(x,end="+")
     print(' =',hasil)
-logic(50)
+logic(20)
